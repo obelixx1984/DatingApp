f16929894b51b1604f322f4f34bc63493943f239
obelixx1984 <karbowiakk@wp.pl>
2022-03-06 11:48:22 +0100

    Koniec Sekcji 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -19,6 +19,7 @@ namespace API
     public class Startup
     {
         private readonly IConfiguration _config;
+        
         public Startup(IConfiguration config)
         {
             _config = config;
@@ -33,6 +34,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -52,6 +54,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
